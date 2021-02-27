e5e8e48b2e634b46ec080d4ee3867e37c5d4d978
XlXi <wagnessgaming2014@gmail.com>
2021-02-27 18:51:38 -0500

    basic index page

--- Backend/Global.php
+++ Backend/Global.php
@@ -1,5 +1,11 @@
 <?php
-$loader = new \Twig\Loader\FilesystemLoader($_SERVER['DOCUMENT_ROOT'] . '/../../Templating');
-$twig = new \Twig\Environment($loader, [
-    'cache' => $_SERVER['DOCUMENT_ROOT'] . '/../../TwigCache',
+
+spl_autoload_register(function($cName)
+{
+	require(__DIR__ . "/../Libraries/" . $cName . ".php");
+});
+
+$loader = new Twig\Loader\FilesystemLoader($_SERVER['DOCUMENT_ROOT'] . '/../../Templating');
+$twig = new Twig\Environment($loader, [
+    //'cache' => $_SERVER['DOCUMENT_ROOT'] . '/../../TwigCache',
 ]);
---
+++ Public/Root/index.php
@@ -0,0 +1,7 @@
+<?php
+
+require($_SERVER["DOCUMENT_ROOT"] . "/../../Backend/Global.php");
+
+echo $twig->render('index.html', ['pageTitle' => 'Test', 'env' => ['Name'=>'Project Zeus']]);
+
+?>
