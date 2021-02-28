759c9dda54102dea534b3bcf0cc6baa107bf91cd
XlXi <wagnessgaming2014@gmail.com>
2021-02-27 20:01:45 -0500

    lumen
    cum

--- Public/Root/index.php
+++ Public/Root/index.php
@@ -2,6 +2,6 @@
 
 require($_SERVER["DOCUMENT_ROOT"] . "/../../Backend/Global.php");
 
-echo $twig->render('index.html', ['pageTitle' => 'Test', 'env' => ['Name'=>'Project Zeus']]);
+echo $twig->render('index.html', ['pageTitle' => 'Test', 'env' => ['Name'=>'Project Zeus', 'Domain'=>['Scheme'=>'http', 'Url'=>'zeus.local']]]);
 
 ?>
