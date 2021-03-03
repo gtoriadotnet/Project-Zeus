1461f54641f67330f2525121da1392e4b6c97083
XlXi <wagnessgaming2014@gmail.com>
2021-03-03 09:40:20 -0500

    error page improvements

--- Backend/Global.php
+++ Backend/Global.php
@@ -5,7 +5,15 @@ spl_autoload_register(function($cName)
 	require(__DIR__ . "/../Libraries/" . $cName . ".php");
 });
 
+use Zeus\IssuePage;
+
 $loader = new Twig\Loader\FilesystemLoader($_SERVER['DOCUMENT_ROOT'] . '/../../Templating');
 $twig = new Twig\Environment($loader, [
     //'cache' => $_SERVER['DOCUMENT_ROOT'] . '/../../TwigCache',
 ]);
+
+if(strpos($_SERVER['REQUEST_URI'], '.php'))
+{
+	http_response_code(404);
+	exit($twig->render('error.html', ['pageTitle' => 'Error', 'env' => IssuePage::IssueEnv(), 'responseCode' => 404]));
+}
