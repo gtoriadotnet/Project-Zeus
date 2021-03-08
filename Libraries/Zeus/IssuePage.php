e78b8cfc562df8303f066ae1364ef79f4d813ea7
XlXi <wagnessgaming2014@gmail.com>
2021-03-08 14:45:41 -0500

    aaaaaaaa

--- Libraries/Zeus/IssuePage.php
+++ Libraries/Zeus/IssuePage.php
@@ -7,7 +7,16 @@ namespace Zeus
 		static public function IssueEnv()
 		{
 			$config = json_decode(file_get_contents(__DIR__ . "/../../zeus.config"));
-			return ['Name'=>$config->name, 'Domain'=>['Scheme'=>$config->domain->scheme, 'Url'=>$config->domain->host]];
+			$alert = $config->alert;
+			if($config->offline == 'True')
+			{
+				$alert = [
+					'active'=>'True',
+					'message'=>$config->name . ' is currently under maintenance.',
+					'type'=>3
+				];
+			}
+			return ['Name'=>$config->name, 'Domain'=>['Scheme'=>$config->domain->scheme, 'Url'=>$config->domain->host], 'Alert'=>$alert];
 		}
 	}
 }
