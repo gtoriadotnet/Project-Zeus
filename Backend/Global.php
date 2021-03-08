cb7e7c0c59d38d923cd812b1ce9abc4989ecb67b
XlXi <wagnessgaming2014@gmail.com>
2021-03-07 23:15:25 -0500

    egg

--- Backend/Global.php
+++ Backend/Global.php
@@ -6,11 +6,17 @@ spl_autoload_register(function($cName)
 });
 
 use Zeus\IssuePage;
+use Zeus\API;
 
 $loader = new Twig\Loader\FilesystemLoader($_SERVER['DOCUMENT_ROOT'] . '/../../Templating');
 $twig = new Twig\Environment($loader, [
     //'cache' => $_SERVER['DOCUMENT_ROOT'] . '/../../TwigCache',
 ]);
+
+if(API::GetSetting('offline')=='True')
+{
+	exit($twig->render('offline.html', ['pageTitle' => 'Maintenance', 'env' => IssuePage::IssueEnv()]));
+}
 
 if(strpos($_SERVER['REQUEST_URI'], '.php'))
 {
