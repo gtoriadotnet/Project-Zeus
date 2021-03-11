8c5b36ed5a609c70fb6e1ad9931f6bc9b806dbd8
XlXi <wagnessgaming2014@gmail.com>
2021-03-11 00:23:15 -0500

    error handling

---
+++ Libraries/Zeus/PageSandboxer.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Zeus
+{
+	class PageSandboxer
+	{
+		public function handleException($severity, $message, $filename, $lineno)
+		{
+			throw new \ErrorException($message, 0, $severity, $filename, $lineno);
+		}
+		
+		public function CreateExceptionHandler()
+		{
+			set_error_handler(array($this, 'handleException'));
+		}
+		
+		public function RunSandbox($sandboxedFunction, $isApi, $twig = null)
+		{
+			try
+			{
+				$sandboxedFunction();
+			}
+			catch(\ErrorException $e)
+			{
+				$debugEnabled = API::GetSetting('debug') == 'True';
+				
+				if($isApi)
+				{
+					if($debugEnabled)
+					{
+						API::Respond(['Error'=>'An internal server error occurred whilst trying to process the request.', 'Trace'=>$e->getTraceAsString()], '500 Internal Server Error');
+						exit;
+					}
+					else
+					{
+						http_response_code(500);
+						require_once(__DIR__ . '/../../Clientapis/ApiErrorShared.php');
+						exit;
+					}
+				}
+				else
+				{
+					if($debugEnabled)
+					{
+						exit($twig->render('error.html', ['pageTitle' => 'Error', 'env' => IssuePage::IssueEnv(), 'responseCode' => 500, 'trace' => $e->getTraceAsString()]));
+					}
+					else
+					{
+						http_response_code(500);
+						require_once(__DIR__ . '/../../Public/Root/Pages/error.php');
+						exit;
+					}
+				}
+				exit($e->getTraceAsString());
+			}
+		}
+	}
+}
--- Public/Root/index.php
+++ Public/Root/index.php
@@ -1,9 +1,21 @@
 <?php
 
 use Zeus\IssuePage;
+use Zeus\PageSandboxer;
 
 require_once($_SERVER['DOCUMENT_ROOT'] . '/../../Backend/Global.php');
 
+$sandbox = new PageSandboxer;
+$sandbox->CreateExceptionHandler();
+
+$sandbox->RunSandbox(
+	function()
+	{
+		$egg = $bruh;
+	},
+	false,
+	$twig
+);
 exit($twig->render('index.html', ['pageTitle' => 'Home', 'env' => IssuePage::IssueEnv()]));
 
 ?>
