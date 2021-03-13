f704776863d50d3b066ccf17b5b1102a5dbcd7af
XlXi <wagnessgaming2014@gmail.com>
2021-03-13 13:18:45 -0500

    fuk u

--- Backend/Global.php
+++ Backend/Global.php
@@ -20,6 +20,6 @@ $pos = strpos($requri, '?');
 if(strpos(substr($requri, 0, ($pos ? $pos : null)), '.php'))
 {
 	http_response_code(404);
-	require_once(__DIR__ . '/../Public/Root/Pages/error.php');
+	require(__DIR__ . '/../Public/Root/Pages/error.php');
 	exit;
 }
